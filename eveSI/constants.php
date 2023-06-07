acb74bb3acc02a17450ae76a2e323162a1ce24ba
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-06 18:12:05 -0600

    Update constants.php

--- eveSI/constants.php
+++ eveSI/constants.php
@@ -47,17 +47,17 @@ const IMAGESERVER = "https://images.evetech.net";
  * @desc The version of the api to use.
  * @desc Versioning
  *    @since v0.0.1a * 
-ESI itself has an API version (as defined in the spec). That version number is mostly irrelevant to API consumers (itís the version of ESI-lib). Instead, ESI versions each route individually. This allows us to make much faster changes and avoid the awkward situation of global API versioning.
+ESI itself has an API version (as defined in the spec). That version number is mostly irrelevant to API consumers (it‚Äôs the version of ESI-lib). Instead, ESI versions each route individually. This allows us to make much faster changes and avoid the awkward situation of global API versioning.
 
-As an example, letís say you have a route /v1/hello/<string>/, and you want to change the path parameter to accepting an integer instead. With a traditional API, the entire APIs basePath would have to be bumped. This is obviously a little less than ideal, considering there could be hundreds of other unchanged routes.
+As an example, let‚Äôs say you have a route /v1/hello/<string>/, and you want to change the path parameter to accepting an integer instead. With a traditional API, the entire APIs basePath would have to be bumped. This is obviously a little less than ideal, considering there could be hundreds of other unchanged routes.
 
 With ESI, you always (and only) get three complete APIs, /dev/, /latest/ and /legacy/. But each route is also given a numbered path, starting with /v1/. Alternate routes are mentioned in the route description (until something better comes along in the standards?).
 
 /dev/ can change at any point, changes to /latest/ will be announced. After changes are made the previous /latest/ will be available as /legacy/, until the next version bump. If you want to avoid the return schema of your request suddenly changing, you can use the versioned alternate route. In that case, prudent developers may want to create unit tests (watch for the warning: 199 headers) to notify them when endpoints are moved to legacy.
 
-Additionally, endpoints may be deprecated. When an endpoint is deprecated, a line appears through it on the Swagger UI, and it begins returning the ìwarning: 299î header. This is slightly different than a warning: 199 header, which you will receive if an endpoint was updated and there is now a newer version of it available. Deprecation is how Tech Co broadcasts an intent to delete a route. Deprecated endpoints may include a recommended alternate source or other message in the 299 warning, and you should move away from them immediately.
+Additionally, endpoints may be deprecated. When an endpoint is deprecated, a line appears through it on the Swagger UI, and it begins returning the ‚Äúwarning: 299‚Äù header. This is slightly different than a warning: 199 header, which you will receive if an endpoint was updated and there is now a newer version of it available. Deprecation is how Tech Co broadcasts an intent to delete a route. Deprecated endpoints may include a recommended alternate source or other message in the 299 warning, and you should move away from them immediately.
 
-Because endpoints are versioned individually, the concept of an overall /v1/ (or 2 or 5) API is not very relevant, and no swagger-ui is provided for these routes. Browsing the APIís capabilities should be done via /dev/, /latest/ or /legacy/. However, users who wish to indulge their curiosity may feed a /v1/swagger.json into their own Swagger UI to get an overview, if they wish.
+Because endpoints are versioned individually, the concept of an overall /v1/ (or 2 or 5) API is not very relevant, and no swagger-ui is provided for these routes. Browsing the API‚Äôs capabilities should be done via /dev/, /latest/ or /legacy/. However, users who wish to indulge their curiosity may feed a /v1/swagger.json into their own Swagger UI to get an overview, if they wish.
 
 As an aside, all ESI routes end with a /. The only exceptions are the /<version>/swagger.json routes and routes used by the swagger-ui, which are passed through ESI.
  * @param   latest
@@ -99,8 +99,14 @@ const ENABLE_SSL = false;
  *     @since v0.0.1a
  */
 const ENABLE_DEBUG = false;
-
-const SWAGGER_JSON = "https://esi.evetech.net/latest/swagger.json"; //Not implimented
+//Not implimented
+/**
+ * @var const SWAGGER_JSON
+ * @desc The URI to location of the Eve Online ESI swagger.json file
+ * @param string URI
+ *     @since v0.0.1a
+ */
+const SWAGGER_JSON = "https://esi.evetech.net/latest/swagger.json";
 
 
 
