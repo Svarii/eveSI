a254e582114afc49582f8088878abef9a93a5786
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-09 19:19:08 -0600

    Update eveSI.php

--- eveSI/eveSI.php
+++ eveSI/eveSI.php
@@ -16,7 +16,7 @@
 namespace eveSI;
 
 // Application Settings
-require_once('constants.php');
+require_once('src/config/core.php');
 
 // Use constants from the Eve Swagger Interface
 use const eveSI\BASEURI; 
