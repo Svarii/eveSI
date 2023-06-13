d2e6f3bf76199102142cce0aac9a214e6dab9d15
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-12 21:28:58 -0600

    Update characters_endpoints.php

--- eveSI/src/endpoints/characters_endpoints.php
+++ eveSI/src/endpoints/characters_endpoints.php
@@ -1,1 +1,18 @@
-
+<?php
+Class publicInformation extends eveSI{
+    function getAssociativeArray() : array{
+        return json_decode($this->_esiRequestHandler('characters', {$character_id}), true);
+    }
+    function getObject() : \stdClass{
+        return json_decode($this->_esiRequestHandler('characters', {$character_id}));
+    }
+}
+Class agentsResearch extends eveSI{
+    function getAssociativeArray() : array{
+        return json_decode($this->_esiRequestHandler('agents_research', {$character_id}), true);
+    }
+    function getObject() : \stdClass{
+        return json_decode($this->_esiRequestHandler('agents_research', {$character_id}));
+    }
+}
+?>
