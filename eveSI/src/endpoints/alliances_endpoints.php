f736abcd4bf5a011f2e1f8a36e5084b0442472c3
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-13 17:49:20 -0600

    Update alliances_endpoints.php

--- eveSI/src/endpoints/alliances_endpoints.php
+++ eveSI/src/endpoints/alliances_endpoints.php
@@ -7,17 +7,15 @@ abstract class convertAPIData extends eveSI {
     protected function getAssociativeArray($id = null) : array {
         return json_decode($this->_esiRequestHandler($this->identifier($id)), true);
     }
-
     protected function getObject($id = null) : \stdClass {
         return json_decode($this->_esiRequestHandler($this->identifier($id)));
     }
-
     protected function getJSONString($id = null) : string {
         return $this->_esiRequestHandler($this->identifier($id));
     }
-
     abstract protected function identifier($id = null);
 }
+
 Class allAlliancesList extends convertAPIData {
     protected function identifier($id = null) {
         return 'alliances';
