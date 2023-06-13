1f316d0f99ee2621dc27a2f239157cc66af166fe
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-12 21:23:36 -0600

    Update _endpoints.php

--- eveSI/src/endpoints/_endpoints.php
+++ eveSI/src/endpoints/_endpoints.php
@@ -64,52 +64,6 @@ Class eveSI_endpoints{
 }
 
 
-
-
-//TODO:: Add this structure/functionality to all files in the endpoints folder
-Class allAlliancesList extends eveSI{
-    function getAssociativeArray() : array{
-        return json_decode($this->_esiRequestHandler('alliances'), true);
-    }
-    function getObject() : \stdClass{
-        return json_decode($this->_esiRequestHandler('alliances'), true);
-    }
-}
-Class allianceCorporations extends eveSI{   
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/corporations'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/corporations'));
-    }
-}
-Class allianceContacts extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/contacts'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/contacts'));
-    }
-}
-Class allianceIcon extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/icons'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/icons'));
-    }
-}
-Class allianceInformation extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}'));
-    }
-}
-
-
-
 Class characters_assets_endpoints{
     public $locations;
     public $names;
