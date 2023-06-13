69f8806a54fd3742390cb8e4f9d1fa72604b742e
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-13 17:30:33 -0600

    Update alliances_endpoints.php

--- eveSI/src/endpoints/alliances_endpoints.php
+++ eveSI/src/endpoints/alliances_endpoints.php
@@ -1,56 +1,46 @@
 
 <?php
 //TODO:: Add this structure/functionality to all files in the endpoints folder
-//TODO: Remove extends, this should not be nessarcy since it's included, you should be able to use $this->_esiRequestHandler
+//TODO:: Ignore previous todo, that guy forgot what I was doing.  I think this guy is back on track.
 
-abstract class convertAPIData {
-    protected abstract function getAssociativeArray($endpoint_path) : array;
-    protected abstract function getObject($endpoint_path) : \stdClass;
-    protected abstract function getJSONString($endpoint_path) : string;
-}
+abstract class convertAPIData extends eveSI {
+    protected function getAssociativeArray() : array {
+        return json_decode($this->_esiRequestHandler($this->identifier()), true);
+    }
 
+    protected function getObject() : \stdClass {
+        return json_decode($this->_esiRequestHandler($this->identifier()));
+    }
 
-Class allAlliancesList extends eveSI{
-    function getAssociativeArray() : array{
-        return json_decode($this->_esiRequestHandler('alliances'), true);
+    protected function getJSONString() : string {
+        return $this->_esiRequestHandler($this->identifier());
     }
-    function getObject() : \stdClass{
-        return json_decode($this->_esiRequestHandler('alliances'), true);
-    }
-    function getJSONString() : string{
-        return ($this->_esiRequestHandler('alliances'));
+
+    abstract protected function identifier($id);
+}
+Class allAlliancesList extends convertAPIData {
+    protected function identifier($id = null) {
+        return 'alliances';
     }
 }
-Class allianceCorporations extends eveSI{   
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/corporations'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/corporations'));
+Class allianceCorporations extends convertAPIData{   
+    protected function identifier($id) {
+        return "alliances/{$id}/corporations";
     }
 }
-Class allianceContacts extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/contacts'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/contacts'));
+Class allianceContacts extends convertAPIData{   
+    protected function identifier($id) {
+        return "alliances/{$id}/contacts";
     }
 }
-Class allianceIcon extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/icons'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}/icons'));
+Class allianceIcon extends convertAPIData{   
+    protected function identifier($id) {
+        return "alliances/{$id}/icons";
     }
 }
-Class allianceInformation extends eveSI{
-    function getAssociativeArray(string $alliance_id) : array{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}'), true);
-    }
-    function getObject(string $alliance_id) : \stdClass{
-        return json_decode($this->esiRequestHandler('alliances/{$alliance_id}'));
+Class allianceInformation extends convertAPIData{   
+    protected function identifier($id) {
+        return "alliances/{$id}/icons";
     }
 }
 ?>
