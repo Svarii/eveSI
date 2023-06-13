db9488a426f5480e724ec400509d6a654e5b0278
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-12 21:48:01 -0600

    Update alliances_endpoints.php

--- eveSI/src/endpoints/alliances_endpoints.php
+++ eveSI/src/endpoints/alliances_endpoints.php
@@ -2,6 +2,7 @@
 <?php
 //TODO:: Add this structure/functionality to all files in the endpoints folder
 //TODO: Remove extends, this should not be nessarcy since it's included, you should be able to use $this->_esiRequestHandler
+//TDOD: Complete Abstract Class for dataconversion methods.
 Class allAlliancesList extends eveSI{
     function getAssociativeArray() : array{
         return json_decode($this->_esiRequestHandler('alliances'), true);
