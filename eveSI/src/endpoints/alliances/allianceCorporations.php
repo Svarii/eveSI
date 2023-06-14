bf30130d44e404698e4e6dd3172246eedaaa4d68
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 17:02:25 -0600

    Update allianceCorporations.php

--- eveSI/src/endpoints/alliances/allianceCorporations.php
+++ eveSI/src/endpoints/alliances/allianceCorporations.php
@@ -1,4 +1,6 @@
 <?php
+namespace eveSI\endpoints\alliances;
+
 Class allianceCorporations extends convertAPIData{   
     protected function identifier($id) {
         return "alliances/{$id}/corporations";
