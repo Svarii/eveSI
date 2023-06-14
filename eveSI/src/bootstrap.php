b93d93e6b48fc2f5a428b727555af7d31ce7312e
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 05:29:30 -0600

    Update bootstrap.php

--- eveSI/src/bootstrap.php
+++ eveSI/src/bootstrap.php
@@ -1,8 +1,32 @@
 <?php
 //Bootstrap
-require_once('config/core.php');
-require_once('eveSI.php');
-require_once('convertData.php');
-require('endpoints/_endpoints.php');
-require('convertAPIData.php');
+//TODO:: This needs a line by line review and testing
+spl_autoload_register(function ($class) {
+    // Project-specific namespace prefix
+    $prefix = 'MyNamespace\\';
+
+    // Base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+
+    // Does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // No, move to the next registered autoloader
+        return;
+    }
+
+    // Get the relative class name
+    $relative_class = substr($class, $len);
+
+    // Replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // If the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
 ?>
