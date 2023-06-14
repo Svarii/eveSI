dd5a737a6b41435a48d00138e1d98bdd96112b6f
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 17:03:01 -0600

    Update allianceInformation.php

--- eveSI/src/endpoints/alliances/allianceInformation.php
+++ eveSI/src/endpoints/alliances/allianceInformation.php
@@ -1,4 +1,6 @@
 <?php
+namespace eveSI\endpoints\alliances;
+
 Class allianceInformation extends convertAPIData{   
     protected function identifier($id) {
         return "alliances/{$id}/icons";
