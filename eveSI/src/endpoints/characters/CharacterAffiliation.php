8a57f061050d9118072e7abc38f622ed9f91e42e
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 19:21:19 -0600

    Create CharacterAffiliation.php

---
+++ eveSI/src/endpoints/characters/CharacterAffiliation.php
@@ -0,0 +1,9 @@
+<?php
+//FIXME::POST Has not been implimented or tested yet
+//Will not work at all.
+class CharacterAffiliation extends eveSIDataConverter {
+    protected function getUrl($listOfIds) {
+        return "characters/affiliation";
+    }
+}
+?>
