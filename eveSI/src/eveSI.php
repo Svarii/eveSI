69c342aa0890c1c87afbc98b5ecf76f1d58082be
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 21:38:18 -0600

    refactor this part soon

--- eveSI/src/eveSI.php
+++ eveSI/src/eveSI.php
@@ -16,6 +16,7 @@
 namespace eveSI;
 
 // Use constants defined in config/core.php
+//FIXME: Centralize, anti-dependant
 use const eveSI\BASEURI; 
 use const eveSI\DATASOURCE;
 use const eveSI\LANGUAGE;
