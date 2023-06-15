d1ed61a7049eeb91085cab306bad02c136fd17a0
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-14 19:22:27 -0600

    Update and rename CharacterAffiliation.php to CharactersAffiliation.php

--- eveSI/src/endpoints/characters/CharactersAffiliation.php
+++ eveSI/src/endpoints/characters/CharactersAffiliation.php
@@ -1,7 +1,7 @@
 <?php
 //FIXME::POST Has not been implimented or tested yet
 //Will not work at all.
-class CharacterAffiliation extends eveSIDataConverter {
+class CharactersAffiliation extends eveSIDataConverter {
     protected function getUrl($listOfIds) {
         return "characters/affiliation";
     }
