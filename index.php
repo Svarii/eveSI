d341337daeee64f5374e143dd1a26f594caf1309
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-16 20:49:05 -0600

    savepoint

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ require_once('eveSI/boostrap.php');
 
 $esi = new eveSI;
 
+$characterName = $esi->characters->publicInformation->name;
+$characterCorporation = $esi->characters->publicInformation->corporation;
 ?>
