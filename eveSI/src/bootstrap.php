5f3d15462c960ed11dedbd71ce8f2453a16118e0
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-16 20:03:44 -0600

    refactoring

--- eveSI/src/bootstrap.php
+++ eveSI/src/bootstrap.php
@@ -1,34 +1,23 @@
 <?php
 //Bootstrap
-require('convertAPIData.php');
-require('eveSI.php');
-//TODO:: This needs a line by line review and testing
-//FIXME:: Review and testing??  What were you smoking.  This is needs a complete rewrite.  It won't work like this...
-spl_autoload_register(function ($class) {
-    // Project-specific namespace prefix
+require_once('eveSI.php');
+require_once('convertAPIData.php');
+
+function eveSIBootstrap($class) {
     $prefix = 'eveSI\\';
-
-    // Base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/';
-
-    // Does the class use the namespace prefix?
+    $base_dir = __DIR__ . '/endpoints/';
     $len = strlen($prefix);
+
     if (strncmp($prefix, $class, $len) !== 0) {
-        // No, move to the next registered autoloader
         return;
     }
 
-    // Get the relative class name
     $relative_class = substr($class, $len);
-
-    // Replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-    // If the file exists, require it
     if (file_exists($file)) {
-        require $file;
+        require_once $file;
     }
-});
+}
 ?>
+
