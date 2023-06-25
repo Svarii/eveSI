a5df6ace486dcba2a89ab762e1182645f4f81b9a
Svarii <mpeplow855@gmail.com>
2023-06-25 03:45:25 -0600

    .

--- eveSI/src/endpoints/characters/assets/characters_assets_locations.php
+++ eveSI/src/endpoints/characters/assets/characters_assets_locations.php
@@ -2,11 +2,10 @@
 namespace eveSI\endpoints\characters;
 
 Class characters_assets_locations extends eveSI{
-    function getAssociativeArray($character_id, $access_token){
-        return json_decode($this->_esiRequestHandler("characters/{$character_id}/assets/locations", $access_token), true);
-    }
-    function getObject($character_id, $access_token){
-        return json_decode($this->_esiRequestHandler("characters/{$character_id}/assets/locations", $access_token));
+    Class allianceContacts extends {   
+        protected function identifier($id) {
+            return "alliances/{$id}/contacts";
+        }
     }
 }
 ?>
