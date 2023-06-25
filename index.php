b98a2cc4488c4643a4cc2bcadd35e22231e4d525
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-25 01:45:57 -0600

    .

--- index.php
+++ index.php
@@ -3,11 +3,14 @@ require_once('eveSI/boostrap.php');
 
 $esi = new eveSI;
 
+//Example Usage
+//$charInfo = $esi->characters->publicInformation->getObject($character_id);
+
 function loadCharacterDetails($character_id){}
     //Use Character Affiliation instead?
     $characterDetails = [];
     $characterDetails['characterName'] = $esi->characters->publicInformation->getObject($character_id)->name;
-    $characterDetails['characterCorporationID'] = $esi->characters->publicInformation->getObject($character_id)->corporation;
+    $characterDetails['characterCorporation'] = $esi->characters->publicInformation->getAsscoaiteArray($characer_id)['corporation'];
     return $characterDetails;
 }
 $characterDetails = loadCharacterDetails($character_id);
