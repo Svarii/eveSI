580bd02e755b3b4168e0650b463d19b12ce8d024
Svarii <mpeplow855@gmail.com>
2023-06-25 03:31:03 -0600

    .

---
+++ eveSI/src/endpoints/characters/characters.php
@@ -0,0 +1,48 @@
+<?php
+namespace eveSI\endpoints\characters;
+
+Class _endpoints{
+    public $agents_research;
+    public $blueprints;
+    public $corporationhistory;
+    public $cspa;
+    public $fatigue;
+    public $medals;
+    public $notifications;
+    public $portrait;
+    public $roles;
+    public $standings;
+    public $titles;
+    public $affiliation;
+    public $assets;
+    public $bookmarks;
+    public $calendar;
+    public $clones;
+    public $implants;
+    public $contacts;
+    public $contracts;
+    public $fitings;
+    public $fleet;
+    public $industry;
+    public $mining;
+    public $facilities;
+    public $killmails;
+    public $locaions;
+    public $online;
+    public $ship;
+    public $loyalty;
+    public $mail;
+    public $orders;
+    public $opporutnities;
+    public $planets;
+    public $search;
+    public $attributes;
+    public $skillqueue;
+    public $skills;
+    public $wallet;
+
+    public function __construct(){
+ 
+    }
+}
+?>
