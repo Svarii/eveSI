f3dc459d36161e08f43bd07a8ed77aed4d217bce
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-26 19:50:34 -0600

    character endpoints update

--- eveSI/src/endpoints/characters/characters.php
+++ eveSI/src/endpoints/characters/characters.php
@@ -33,7 +33,7 @@ Class _endpoints{
     public $loyalty;
     public $mail;
     public $orders;
-    public $opporutnities;
+    public $opportunities;
     public $planets;
     public $search;
     public $attributes;
@@ -42,7 +42,43 @@ Class _endpoints{
     public $wallet;
 
     public function __construct(){
- 
+        $agents_research = new eveSI\endpoints\characters\agents_research;
+        $blueprints = new eveSI\endpoints\characters\blueprints;
+        $corporationhisotry = new eveSI\endpoints\characters\corporationhistory;
+        $cspa = new eveSI\endpoints\characters\cspa;
+        $fatigue = new eveSI\endpoints\characters\fatigue;
+        $medals = new eveSI\characters\medals;
+        $notifications = new eveSI\characters\notifications;
+        $portrait = new eveSI\characters\portrait;
+        $roles = new eveSI\characters\roles;
+        $standings = new eveSI\characters\standings;
+        $titles = new eveSI\characters\titles;
+        $affiliation = new eveSI\characters\affiliation;
+        $assets = new eveSI\characters\assets;
+        $bookmarks = new eveSI\characters\bookmarks;
+        $calendar = new eveSI\characters\calendar;
+        $clones = new eveSI\characters\clones;
+        $implants = new eveSI\characters\implants;
+        $contacts = new eveSI\characters\contacts;
+        $contracts = new eveSI\characters\contracts;
+        $fittings = new eveSI\characters\fittings;
+        $fleet = new eveSI\characters\fleet;
+        $industry = new eveSI\characters\industry;
+        $mining = new eveSI\characters\mining;
+        $facilities = new eveSI\characters\facilities;
+        $killmails = new eveSI\characters\killmails;
+        $locations = new eveSI\characters\locations;
+        $online = new eveSI\characters\online;
+        $ship = new eveSI\characters\ship;
+        $loyalty = new eveSI\characters\loyalty;
+        $mail = new eveSI\characters\mail;
+        $orders = new eveSI\characters\orders;
+        $opportunities = new eveSI\characters\opportunities;
+        $planets = new eveSI\characters\planets;
+        $search = new eveSI\characters\search;
+        $skillqueue = new eveSI\characters\skillqueue;
+        $skills = new eveSI\characters\skills;
+        $wallet = new eveSI\characters\wallet;
     }
 }
 ?>
