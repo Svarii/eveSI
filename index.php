70ec3faf59eb01bdd7503e1ff56aec26adaa53f1
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-27 01:25:25 -0600

    .

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ echo 'All systems online. Awaiting commands.';
 // Example Usage
 //$charInfo = $esi->characters->publicInformation->getObject($character_id);
 
-function loadCharacterDetails($character_id){
+function loadCharacterDetails($character_id, $esi){
     //Use Character Affiliation instead?
     $characterDetails = [];
     $characterDetails['characterName'] = $esi->characters->publicInformation->getObject($character_id)->name;
     $characterDetails['characterCorporation'] = $esi->characters->publicInformation->getAsscoaiteArray($characer_id)['corporation'];
     return $characterDetails;
 }
-$characterDetails = loadCharacterDetails($character_id);
+$characterDetails = loadCharacterDetails($character_id, $esi);
 var_dump($characterDetails);
 ?>
