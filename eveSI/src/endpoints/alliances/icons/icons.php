592e7521e814b4cbf30b8990611c1665e3a7abef
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-26 23:42:22 -0600

    Update icons.php

--- eveSI/src/endpoints/alliances/icons/icons.php
+++ eveSI/src/endpoints/alliances/icons/icons.php
@@ -1,7 +1,7 @@
 <?php
 namespace eveSI\endpoints\alliances\icons;
 
-Class icons extends convertAPIData{   
+Class icons extends eveSI{   
     protected function identifier($id) {
         return "alliances/{$id}/icons";
     }
