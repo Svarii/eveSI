52c43bbd62949801912135fd840cadb228dbd1f2
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-27 23:14:37 -0600

    save

---
+++ src/eveSI/endpoints/universe/races/races.php
@@ -0,0 +1,9 @@
+<?php
+namespace eveSI\endpoints\universe\races;
+
+Class races extends eveSI{
+    protected function identifier(){
+        return "universe/races";
+    }
+}
+?>
---
+++ src/eveSI/endpoints/universe/regions/regions.php
@@ -0,0 +1,9 @@
+<?php
+namespace eveSI\endpoints\universe\regions;
+
+Class regions extends eveSI{
+    protected functions identifier(){
+        return "universe/regions";
+    }
+}
+?>
