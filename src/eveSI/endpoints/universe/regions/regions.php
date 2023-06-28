69b4d1d4d74647cdfbb5b877d96e931072daa57f
Svarii <58209586+Svarii@users.noreply.github.com>
2023-06-27 23:19:18 -0600

    save

--- src/eveSI/endpoints/universe/regions/regions.php
+++ src/eveSI/endpoints/universe/regions/regions.php
@@ -2,8 +2,12 @@
 namespace eveSI\endpoints\universe\regions;
 
 Class regions extends eveSI{
-    protected functions identifier(){
-        return "universe/regions";
+    protected functions identifier($region_id = null){
+        if ($region_id !==null){
+            return "universe/regions";
+        }else{
+            return "universe/regions/{$region_id}";
+        }
     }
 }
 ?>
